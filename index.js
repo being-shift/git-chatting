1d50c7a8f73ad55105d08e70f34f7aef6d9784a7
being-shift <being_shift@icloud.com>
2023-10-18 22:46:18 +0900

    오타 수정

--- index.js
+++ index.js
@@ -2,18 +2,17 @@
 const {createServer} = require("http");	// protocol 제약
 const app = require("./app");			// 환경변수,connection셋팅 등
 const {Server} = require("socket.io");	
-
+require('dotenv').config();
 const httpServer = createServer(app);	
 const io = new Server(httpServer, {		// 패킷 BUS: 3000번으로 27017과 통신
 	cors: {
 		origin: "http://localhost:3000",
 	}
-}
+});
 
 // 기능분리
 require("./utils/io")(io);
 // web <-> socket 연결
-require('dotenv').config();
 httpServer.listen(process.env.PORT,		// 5001 -> 3000 <-> 27017 
 	() => { console.log("server listening on port", process.env.PORT); }
 );
